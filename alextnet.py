bef39f090494b97501acfb9cb4faaf499b8a26bc
ChuongLe279 <lebuuchuong2024@gmail.com>
2026-09-20 20:27:26 +0700

    Initialize class name

---
+++ alextnet.py
@@ -0,0 +1,18 @@
+class ConvolutionalLayer:
+    def __init__(self) -> None:
+        pass
+
+class FullyConnectedLayer:
+    def __init__(self) -> None:
+        pass
+
+class PoolingLayer:
+    def __init__(self) -> None:
+        pass
+
+class AlexNet:
+    def __init__(self) -> None:
+        pass
+
+if __name__ == "__main__":
+    pass
